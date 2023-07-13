98e4e4c5545d38172eb8c31f5f6948ea97ab46e1
huccct <2972223145@qq.com>
2023-07-13 12:31:40 +0800

    feat: 🎸 init project

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { message } from 'antd'
 let request = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
   timeout: 5000
@@ -23,36 +23,33 @@ request.interceptors.response.use(
     }
   },
   (error) => {
-    let message = ''
+    let messageContent = ''
     let status = error.response.status
     switch (status) {
       // 401: 未登录
       // 未登录则跳转登录页面，并携带当前页面的路径
       // 在登录成功后返回当前页面，这一步需要在登录页操作。
       case 401:
-        message = '未登录'
+        messageContent = '未登录'
         break
       // 403 token过期
       // 登录过期对用户进行提示
       // 清除本地token和清空vuex中token对象
       // 跳转登录页面
       case 403:
-        message = '登录过期，请重新登录'
+        messageContent = '登录过期，请重新登录'
         break
       case 404:
-        message = '网络请求不存在'
+        messageContent = '网络请求不存在'
         break
       case 500:
-        message = '服务器出现问题'
+        messageContent = '服务器出现问题'
         break
       default:
-        message = error.response.data.message
+        messageContent = error.response.data.message
         break
     }
-    // messageApi.open({
-    //   type: 'error',
-    //   content: message
-    // })
+    message.error(messageContent)
     return Promise.reject(error)
   }
 )
