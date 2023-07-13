dd23267bccbd75c3bbefebb2302f26249c41a71a
huccct <2972223145@qq.com>
2023-07-13 11:56:02 +0800

    feat: 🎸 init project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, UserConfigExport, defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import UnoCSS from 'unocss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), UnoCSS()]
-})
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  let env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [react(), UnoCSS()],
+    resolve: { alias: { '@': path.resolve('./src') } },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";'
+        }
+      }
+    },
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVE,
+          // 需要代理跨域
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
+}
