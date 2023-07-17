3a9cebe4a6562be7f95e5b61894a647416a0b5eb
huccct <2972223145@qq.com>
2023-07-17 15:51:49 +0800

    feat: 🎸 update product/trademark action

---
+++ src/api/product/trademark/index.ts
@@ -0,0 +1,25 @@
+import request from '@/utils/request'
+import { TradeMark, TradeMarkResponseData } from './type'
+
+enum API {
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
+  ADDTRADEMARK_URL = '/admin/product/baseTrademark/save',
+  UPDATETRADEMARK_URL = '/admin/product/baseTrademark/update',
+  DELETE_URL = '/admin/product/baseTrademark/remove/'
+}
+
+export const reqHasTradeMark = (page: number, limit: number) =>
+  request.get<any, TradeMarkResponseData>(
+    API.TRADEMARK_URL + `${page}/${limit}`
+  )
+
+export const reqAddOrUpdateTrademark = (data: TradeMark) => {
+  if (data.id) {
+    return request.put<any, any>(API.UPDATETRADEMARK_URL, data)
+  } else {
+    return request.post<any, any>(API.ADDTRADEMARK_URL, data)
+  }
+}
+
+export const reqDeleteTrademark = (id: number) =>
+  request.delete<string, any>(API.DELETE_URL + id)
--- src/pages/product/trademark/index.tsx
+++ src/pages/product/trademark/index.tsx
@@ -1,8 +1,120 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
+import {
+  reqHasTradeMark,
+  reqAddOrUpdateTrademark,
+  reqDeleteTrademark
+} from '@/api/product/trademark'
+import { Button, Card, Space, Table, Tag } from 'antd'
+import { PlusCircleOutlined } from '@ant-design/icons'
+import type { ColumnsType } from 'antd/es/table'
+import { EditOutlined } from '@ant-design/icons'
+import { DeleteOutlined } from '@ant-design/icons'
+import { Records, TradeMarkResponseData } from '@/api/product/trademark/type'
+import './styles/index.scss'
+interface DataType {
+  id: number
+  tmName: string
+  logoUrl: string
+  key: string
+}
+
+const columns: ColumnsType<DataType> = [
+  {
+    title: '序号',
+    dataIndex: 'id',
+    key: 'id',
+    align: 'center'
+  },
+  {
+    title: '品牌名称',
+    dataIndex: 'tmName',
+    key: 'tmName',
+    align: 'center'
+  },
+  {
+    title: '品牌LOGO',
+    key: 'logoUrl',
+    render: (_, record) => (
+      <>
+        <img src={record.logoUrl} alt="" className="w-100px h-100px" />
+      </>
+    ),
+    align: 'center'
+  },
+  {
+    title: '品牌操作',
+    render: (_, record) => (
+      <>
+        <Button type="primary" icon={<EditOutlined />}></Button>
+        <Button danger icon={<DeleteOutlined />} className="ml-10px"></Button>
+      </>
+    ),
+    align: 'center'
+  }
+]
 
 const index = observer(() => {
-  return <div>trademark</div>
+  const [pageNo, setPageNo] = useState(1)
+  const [limit, setLimit] = useState(3)
+  const [total, setTotal] = useState(0)
+  const [tradeMarkArr, setTradeMarkArr] = useState<Records>([])
+  type TablePaginationPosition =
+    | 'topLeft'
+    | 'topCenter'
+    | 'topRight'
+    | 'bottomLeft'
+    | 'bottomCenter'
+    | 'bottomRight'
+  const getHasTradeMark = async (pageNo: number, pageSize: number) => {
+    let res: TradeMarkResponseData = await reqHasTradeMark(pageNo, pageSize)
+    console.log(pageNo)
+
+    if (res.code === 200) {
+      setTotal(res.data.total)
+      setTradeMarkArr(res.data.records)
+    }
+  }
+  useEffect(() => {
+    getHasTradeMark(pageNo, limit)
+  }, [pageNo, limit])
+
+  const paginationConfig = {
+    pageSize: 3,
+    total: total,
+    showSizeChanger: true,
+    position: ['bottomCenter' as TablePaginationPosition],
+    showTotal: (total: any, range: any) =>
+      `显示 ${range[0]}-${range[1]} 条，共 ${total} 条`
+  }
+  const data: any = tradeMarkArr.map((obj, index) => {
+    return { ...obj, key: obj.id }
+  })
+  const handlePaginationChange = (
+    pagination: any,
+    filters: any,
+    sorter: any
+  ) => {
+    setPageNo(pagination.current)
+    setLimit(pagination.pageSize)
+
+    getHasTradeMark(pagination.current, pagination.pageSize)
+  }
+  return (
+    <>
+      <Card>
+        <Button type="primary" icon={<PlusCircleOutlined />}>
+          添加品牌
+        </Button>
+        <Table
+          dataSource={data}
+          columns={columns}
+          pagination={paginationConfig}
+          onChange={handlePaginationChange}
+        />
+      </Card>
+    </>
+  )
 })
 
 export default index
